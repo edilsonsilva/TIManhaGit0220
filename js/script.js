4397ba78cdb8cf78205722901cd8f1f0fdbe597c
Edilson Silva <edilsonbackend@gmail.com>
2021-05-27 10:01:14 -0300

    link do javascript com html e inicio dos comandos em javascript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var b1 = document.getElementById("btn1");
+var b2 = document.getElementById("btn2");
+var b3 = document.getElementById("btn3");
+
+b1.onclick = ()=>{
+    alert("Você clicou no link 1");
+}
